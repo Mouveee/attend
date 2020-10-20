bfbf9e2e41f8ef190eff6360c1ca79e9b032fbc5
mouveee <mouvy@web.de>
2020-10-20 23:06:52 +0200

    Updates

---
+++ precache-manifest.f3ad2dde83e037cbd4892320989478c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778c11ca1248424a8565f99691f5506a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3cc391f7bc7fc9ccfbf3",
+    "url": "/my-app/static/css/main.ab858fe4.chunk.css"
+  },
+  {
+    "revision": "43c1e4829d79c4d0b5e6",
+    "url": "/my-app/static/js/2.1dd35244.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/my-app/static/js/2.1dd35244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cc391f7bc7fc9ccfbf3",
+    "url": "/my-app/static/js/main.29f575f6.chunk.js"
+  },
+  {
+    "revision": "348facaaa4311f2eb91a",
+    "url": "/my-app/static/js/runtime-main.3e9a3b2b.js"
+  },
+  {
+    "revision": "77c49338f87054c630655d41b46952f0",
+    "url": "/my-app/static/media/MS_EXAM.77c49338.png"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/my-app/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "ed4194764874ccfe89aac021b614877d",
+    "url": "/my-app/static/media/head.ed419476.mp4"
+  }
+]);
