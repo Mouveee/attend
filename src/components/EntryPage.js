4246a3c868b88dba5b1a3c9dc819ecc1207b5b48
mouveee <mouvy@web.de>
2020-10-21 19:05:48 +0200

    i am an idiot and removed one line

--- src/components/EntryPage.js
+++ src/components/EntryPage.js
@@ -8,17 +8,16 @@ export default function EntryPage(props) {
       className={styles.container}
     >
       <div className={styles.content}>
-        Hi, mein Name ist Marco Huwig, ich bin Web Developer und lebe in Saarbrücken. 
+        Hallo, mein Name ist Marco Huwig, ich bin Web Developer und lebe in Saarbrücken. 
         <br></br><br></br>
-        Ich habe mich 2017 vollends auf die Entwicklung von Software im Web spezialiert und 2019
-        an der IHK Saar meinen Abschluss an der IHK Saar gemacht. Mein Abschlussprojekt war ein
-        Microsoft basiertes Projekt in der Azure Cloud, das sowohl Front- als auch Backend Coding
-        beinhaltete. 
+        Ich habe mich 2017 auf die Webentwicklung spezialisiert und 2019
+        meinen Abschluss an der IHK Saar gemacht. In der <i>Microsoft Cloud</i> (Azure, SharePoint,
+        PowerApps zum Beispiel) habe ich bereits mehrere Auftragsrojekte mit <i>Experts Inside</i> umgesetzt
+        und dabei sowohl mit Frontend Entwicklung und Coding im Backend, konkret REST APIs,
+        Erfahrungen gesammelt.  
         <br></br><br></br>
         Ich bin immer auf der Suche nach Möglichkeiten, meine Fähigkeiten und Kenntnisse
-        nützlich einzubringen und freue mich über jede <a href="mailto:huwig.marco@gmail.de">Kontaktaufnahme</a>.
-        Insbesondere zu allem was die Themen HTML, CSS und JavaScript und den gigantischen Kosmos, der sie 
-        umgibt, betrifft. 
+        nützlich einzubringen und freue mich über jeden <a href="mailto:huwig.marco@gmail.de">Kontakt</a>.
       </div>
     </section>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,9 @@ import styles from './NavBar.module.css'
 
 import DehazeIcon from '@material-ui/icons/Dehaze';
 
-
-
 export default function NavBar(props) {
     const handleClick = () => {
-        alert('why? why did you click me?')
+        alert('where is the navbar?')
     }
 
     return (
